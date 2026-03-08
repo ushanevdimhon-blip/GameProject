6469168061d858a2da224ec8b428d936fc16e20f
Дмитрий Ушанёв <ushanevdimhon@gmail.com>
2026-03-08 18:32:11 +0500

    Добавьте файлы проекта.

---
+++ Game1.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGameLibrary;
+
+namespace GameProject
+{
+    public class Game1 : Core
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        public Game1() : base("Game Project", 1280, 720, false)
+        {
+            
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}//1.mvp закинуть изобр гг и добавить базовую физику(бег, прыжок)
---
+++ Program.cs
@@ -0,0 +1,2 @@
+﻿using var game = new GameProject.Game1();
+game.Run();
